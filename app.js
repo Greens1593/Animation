9733aef4d5f0f4d2f44aa6df9ea32cf4b8829d33
Mike Green <greens1593@gmail.com>
2022-09-23 19:44:22 +0300

    Code was added to the function slides

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide');
-
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        
-        slide.classList.add('active')
-    })
+const slides = (numberOfStartCart=0) => {
+    const slides = document.querySelectorAll('.slide');
+    
+    slides[numberOfStartCart].classList.add('active')
+    
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            
+            slide.classList.add('active')
+        })
+    }
+    
+    function clearActiveClasses(){
+        slides.forEach((slide)=>{
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses(){
-    slides.forEach((slide)=>{
-        slide.classList.remove('active')
-    })
-}
+slides()
